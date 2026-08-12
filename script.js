fead8592f092507172367d61b4ace22f06d30be8
marcimotoo <marcel.niemann@outlook.com>
2026-08-12 21:29:38 +0200

    Change call

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
 const shipName = "Starfall";
 let shipHealth = 100;
-let shipInventory = ["Repair Kit", "Repair Kit"];
+let repairKits = 2 ;
 let shipCredits = 1000;
 
 
 function showStatus () {
-    console.log(shipName, "Health " + shipHealth, shipInventory, "Credits " + shipCredits );
+    console.log(shipName, "Status: " + shipHealth + "%:", "Repair Kits (" + repairKits + "):" , "Credits " + shipCredits + "$");
+    document.getElementById('status').innerHTML = '<h2>haha</h2>';
 }
 
 
 function buyRepairKit() {
     if  (shipCredits > 200) {
-        shipCredits = shipCredits - 200;
-        shipInventory.push ("Repair Kit");
-        console.log("Repair Kit added to your Inventory! Credits left = " + shipCredits + " Credits");
+        shipCredits = shipCredits - 900;
+        repairKits = repairKits + 1
+        console.log("Repair Kit added to your Inventory! " + shipCredits + "$ Credits left");
+        // getStatus();
     }
     else {
         shipCredits < 200;
-        console.log ("Not enouth Credits");
+        console.log ("Not enouth $ Credits");
     }    
 }
 
 function shipDamage() {
-    shipHealth.remove = -20;
+    if (shipHealth >= 1) {
+        shipHealth = shipHealth - 25;
+        if (shipHealth > 0) {
+            console.log("Ship is taking damage!! " + shipHealth + "%");
+        }
+        if (shipHealth <= 0) {
+            console.log ("You Died!");
+        }
+    }
 }
 
 function useRepairKit() {
-    shipHealth = shipHealth + 25;
-    if (shipHealth >= 100) {
-        shipHealth = 100;
-        shipInventory.pop("Reapair Kit");
+    if (shipHealth <= 100, repairKits >= 1) {
+        repairKits = repairKits - 1;
+        shipHealth = shipHealth + 25;
+        console.log("Ship repaired! Shiphall = " + shipHealth + "%" );
+        
     } 
-    console.log("Ship repaired = " + "shiphall " + shipHealth );
+    else {
+        console.log("No Repair Kits left")
+    }
+
+
 }
 
 showStatus();
-shipDamage();
-buyRepairKit();
-showStatus();
-useRepairKit();
-showStatus();
+
+// showStatus();
+// buyRepairKit();
+// buyRepairKit();
+// showStatus();
+// shipDamage();
+// shipDamage();
+// useRepairKit();
+// useRepairKit();
+// showStatus();
+// shipDamage();
+// shipDamage();
+// useRepairKit();
+// useRepairKit();
+// showStatus();
+// shipDamage();
+// shipDamage();
+// shipDamage();
