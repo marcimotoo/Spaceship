571cb77117513ed598420a57a771c87cd8dbbf1a
marcimotoo <marcel.niemann@outlook.com>
2026-08-13 00:23:15 +0200

    style.css added

--- script.js
+++ script.js
@@ -1,70 +1,61 @@
 const shipName = "Starfall";
 let shipHealth = 100;
-let repairKits = 2 ;
+let repairKits = 2;
 let shipCredits = 1000;
 
+document.getElementById("status_table").innerHTML = `
+        <table>
+            <tr>
+                <th>Name</th>
+                <td>${shipName}</td>
+           </tr>
+            <tr>
+                <th>Leben</th>
+                <td>${shipHealth}</td>
+           </tr>
+            <tr>
+                <th>Repair Kits</th>
+                <td>${repairKits}</td>
+           </tr>
+            <tr>
+                <th>Credits</th>
+                <td>${shipCredits}</td>
+            </tr>
+         </table>`;
 
-function showStatus () {
-    console.log(shipName, "Status: " + shipHealth + "%:", "Repair Kits (" + repairKits + "):" , "Credits " + shipCredits + "$");
-    document.getElementById('status').innerHTML = '<h2>haha</h2>';
-}
-
+document.getElementById("status_table").classList.toggle("table");
+document.getElementById("status_table").classList.toggle("m_auto");
 
 function buyRepairKit() {
-    if  (shipCredits > 200) {
-        shipCredits = shipCredits - 900;
-        repairKits = repairKits + 1
-        console.log("Repair Kit added to your Inventory! " + shipCredits + "$ Credits left");
-        // getStatus();
-    }
-    else {
-        shipCredits < 200;
-        console.log ("Not enouth $ Credits");
-    }    
+  if (shipCredits > 200) {
+    shipCredits = shipCredits - 900;
+    repairKits = repairKits + 1;
+    console.log("Repair Kit added to your Inventory! " + shipCredits + "$ Credits left");
+    // getStatus();
+  } else {
+    shipCredits < 200;
+    console.log("Not enouth $ Credits");
+  }
 }
 
 function shipDamage() {
-    if (shipHealth >= 1) {
-        shipHealth = shipHealth - 25;
-        if (shipHealth > 0) {
-            console.log("Ship is taking damage!! " + shipHealth + "%");
-        }
-        if (shipHealth <= 0) {
-            console.log ("You Died!");
-        }
+  if (shipHealth >= 1) {
+    shipHealth = shipHealth - 25;
+    if (shipHealth > 0) {
+      console.log("Ship is taking damage!! " + shipHealth + "%");
     }
+    if (shipHealth <= 0) {
+      console.log("You Died!");
+    }
+  }
 }
 
 function useRepairKit() {
-    if (shipHealth <= 100, repairKits >= 1) {
-        repairKits = repairKits - 1;
-        shipHealth = shipHealth + 25;
-        console.log("Ship repaired! Shiphall = " + shipHealth + "%" );
-        
-    } 
-    else {
-        console.log("No Repair Kits left")
-    }
-
-
+  if ((shipHealth <= 100, repairKits >= 1)) {
+    repairKits = repairKits - 1;
+    shipHealth = shipHealth + 25;
+    console.log("Ship repaired! Shiphall = " + shipHealth + "%");
+  } else {
+    console.log("No Repair Kits left");
+  }
 }
-
-showStatus();
-
-// showStatus();
-// buyRepairKit();
-// buyRepairKit();
-// showStatus();
-// shipDamage();
-// shipDamage();
-// useRepairKit();
-// useRepairKit();
-// showStatus();
-// shipDamage();
-// shipDamage();
-// useRepairKit();
-// useRepairKit();
-// showStatus();
-// shipDamage();
-// shipDamage();
-// shipDamage();
